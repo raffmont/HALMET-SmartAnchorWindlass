09625c885ea5c85f07bab949dc00f1223c41dcb1
Matti Airas <matti.airas@hatlabs.fi>
2025-04-24 10:51:23 +0300

    Fix tacho config

--- src/halmet_digital.cpp
+++ src/halmet_digital.cpp
@@ -18,7 +18,7 @@ FloatProducer* ConnectTachoSender(int pin, String name) {
   char config_title[80];
   char config_description[80];
 
-  snprintf(config_path, sizeof(config_path), "", name.c_str());
+  snprintf(config_path, sizeof(config_path), "/Tacho %s/Pin", name.c_str());
   snprintf(config_title, sizeof(config_title), "Tacho %s Pin", name.c_str());
   snprintf(config_description, sizeof(config_description), "Tacho %s Input Pin",
            name.c_str());
@@ -36,6 +36,10 @@ FloatProducer* ConnectTachoSender(int pin, String name) {
   snprintf(config_description, sizeof(config_description),
            "Tacho %s Multiplier", name.c_str());
   auto tacho_frequency = new Frequency(kDefaultFrequencyScale, config_path);
+
+  ConfigItem(tacho_frequency)
+      ->set_title(config_title)
+      ->set_description(config_description);
 
   tacho_input->connect_to(tacho_frequency);
 
